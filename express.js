5e77f08b3f55b5b672708b95e807387685a74ba2
Daniel Cintron <69943020+DevDan13@users.noreply.github.com>
2020-11-14 11:16:07 -0500

    added customer data and route handlers in express.js

--- express.js
+++ express.js
@@ -12,7 +12,46 @@ app.use(express.json());
 
 //Customer reservation information
 
-//let customers
+let currentCust = [
+    {
+        id: "Gavin56",
+        name: "Gavin 'O'Brien O'Scott' O'Brien",
+        email: "TheOBriens@gmail.com",
+        phone: 555-555-5555
+    }
+]
+
+//customer waiting list information
+
+let waitingCust = [
+    {
+        id: "D'Argenio",
+        name: "Amanda 'Panda' D'Argernio",
+        email: "AmandaPanda@gmail.com",
+        phone: 555-555-5555
+    }
+]
+
+//route handles Get and Post
+
+//send to home age by default?
+app.get("/",function(req, res){
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/reserve",function(req, res){
+    res.sendFile(path.join(__dirname, "reserve.html"));
+});
+
+app.get("/tables",function(req, res){
+    res.sendFile(path.join(__dirname, "tables.html"));
+});
+
+
+//displays all reserved customers
+app.get("/api/currentCust", function(req, res) {
+    return res.json(currentCust);
+  });
 
 app.listen(PORT, function(){
     console.log("Server listening on: http://localhost:" + PORT);
